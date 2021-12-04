05005f4654a9768898e24812ca7cd9651cc7f78c
umer farooq <urfq598@gmail.com>
2021-12-04 12:58:56 +0500

    finish project

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1>MONSTERS ROLODEX</h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={this.handleChange}
